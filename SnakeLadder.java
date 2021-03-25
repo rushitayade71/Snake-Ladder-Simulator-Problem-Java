01e73dd5c7fb69db9b457ba819924c3f1f457415
rushitayade71 <rushitayade71@gmail.com>
2021-03-25 23:08:34 +0530

    Added: Check Player Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,8 +8,35 @@ public class SnakeLadder
 		System.out.println("Welcome to the Snake Ladder Simulator Problem  " + " PlayerStartPoint:" +startpoint);
 		Random random = new Random();
 		int Dieroll = (random.nextInt(6))+1;
-
+		{
 		System.out.println("Die number: " +Dieroll);
+		}
+
+		Random rand = new Random();
+                int Check= rand.nextInt(3);
+
+		int playerposition =0;
+
+                switch (Check)
+                        {
+                case 0 :
+                playerposition = playerposition;
+                System.out.println("player same position");
+		break;
+
+                case 1 :
+                playerposition  = Dieroll + playerposition;
+                System.out.println("player move ahead: " + Dieroll + " position.");
+                break;
+
+                case 2 :
+                playerposition = playerposition - Dieroll;
+                System.out.println(" player move back" );
+                break;
+
+                default:
+                 System.out.println("Invalid option");
+			}
 
 	}
 }
