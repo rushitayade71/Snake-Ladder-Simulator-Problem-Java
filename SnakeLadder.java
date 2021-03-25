e5b1f4aa45034180056de47029fd529435fe94a7
rushitayade71 <rushitayade71@gmail.com>
2021-03-25 19:30:02 +0530

    Added: Die Roll And get number between 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+
 public class SnakeLadder
 {
 	public static void main(String grgs[])
 	{
 		int startpoint = 0;
 		System.out.println("Welcome to the Snake Ladder Simulator Problem  " + " PlayerStartPoint:" +startpoint);
+		Random random = new Random();
+		int Dieroll = (random.nextInt(6))+1;
+
+		System.out.println("Die number: " +Dieroll);
+
 	}
 }
