d37cf3f5d72c48763ef1e8dce504ae56d9efc0b9
rushitayade71 <rushitayade71@gmail.com>
2021-03-26 11:41:10 +0530

    Added: Player Final Winning Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,6 +7,7 @@ public class SnakeLadder
 		int startpoint = 0;
 
 		int playerposition =0;
+		int playerpreviousposition = 0;
 
 		System.out.println("Welcome to the Snake Ladder Simulator Problem  " + " PlayerStartPoint:" +startpoint);
 
@@ -16,35 +17,47 @@ public class SnakeLadder
 
 		Random random = new Random();
 		int Dieroll = (random.nextInt(6))+1;
-		{
-		System.out.println("Die number: " +Dieroll);
-		}
+
+		//System.out.println("Die number: " +Dieroll);
+
 
 		Random rand = new Random();
                 int Check= rand.nextInt(3);
+		playerpreviousposition = playerposition;
 
                 switch (Check)
                         {
                 case 0 :
                 playerposition = playerposition;
-                System.out.println("player same position");
+                //System.out.println("player same position");
 		break;
 
                 case 1 :
                 playerposition  = Dieroll + playerposition;
-                System.out.println("player move ahead: " + Dieroll + " position.");
+                //System.out.println("player move ahead: " + Dieroll + " position.");
                 break;
 
                 case 2 :
                 playerposition = playerposition - Dieroll;
-                System.out.println(" player move back" );
+                //System.out.println(" player move back" );
                 break;
 
                 default:
                 System.out.println("Invalid option");
 									}
+
+		if (playerposition < 0){
+			playerposition = startpoint;}
+
+		else if (playerposition > 100){
+			playerposition = playerpreviousposition;}
+
+		else if (playerposition == 100){
+		break;}
+
 					}
-					 System.out.println("Player Win");
+				System.out.println("Player final Position: "  +playerposition);
+				System.out.println("Player Win");
 
 		}
 }
